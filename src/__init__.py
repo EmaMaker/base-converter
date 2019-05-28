a2038da790fd67b8cf4f5bf19e9a4e94169cd1a3
EmaMaker <emamaker0@gmail.com>
2019-05-28 10:07:47 +0200

    added cp2

--- src/__init__.py
+++ src/__init__.py
@@ -6,9 +6,9 @@ def decimalToBase(sNum, sNewBase):
     sNewBase.capitalize()
 
     lNewNum = []
-
+    dNum = int(sNum)
+    
     if sNewBase == "BCD":
-
         for i in range(0, len(sNewBase)):
             #converts every digit into a 4bit binary number
             bcd = decimalToBase(str(list(str(sNum))[i]), "2")
@@ -24,12 +24,37 @@ def decimalToBase(sNum, sNewBase):
             #adds the numbers in the list
             for i in bcd:
                 lNewNum.append(i)
-    else:
-        dNum = int(sNum)
-        dNewBase = int(sNewBase)
+    elif sNewBase == "CP2":
+        lBin = list(decimalToBase(str(abs(int(dNum))), "2"))
+        lBin.reverse()
+        lBin.append(0)
+        lBin.reverse()
+            
+        if dNum >= 0:
+            return ''.join(str(e) for e in lBin)
+        else:
+            lBinNeg = []
+            
+            gotOne = False
+            for e in lBin:
+                if gotOne == False:
+                    lBinNeg.append(e)
+                    if e == '1':
+                        gotOne = True
+                    else:
+                        lBinNeg.append(1 - int(e))
 
+            lBinNeg.reverse()
+            return ''.join(str(n) for n in lBinNeg)
+            
+                    
+            
+            
+            
+    else:
         #number divided by the base until it's 0. rests of the divisions are put in the list
 
+        dNewBase = int(sNewBase)
         while dNum != 0:
             lNewNum.append(dNum % dNewBase)
             dNum = int(dNum / dNewBase)
@@ -66,9 +91,32 @@ def baseToDecimal(sNum, sBase):
 
         #returns the list a single string for the number
         return ''.join(str(e) for e in num)
+    elif sBase == "CP2":
+        #creates empty array and cast the base to an int
+        lNumbs = []
+        
+        #for every char in the string, make it an int. If it's a letter, translate it into ASCII int for the letter and subtract 55  (A-->10 and so on)
+        for c in sNum:
+            try:
+                c1 = int(c)
+            except ValueError:
+                c1 = ord(c) - 55
 
+            lNumbs.append(c1)
+
+        #the list is reversed, so first digit on number is index 0
+        lNumbs.reverse()
+
+        lNum = int(0)
+        #every index of the list is multiplied by the base on the number (Index 0 is base^0 and so on)
+        for a in range(0, len(lNumbs)):
+            if(a+1 == len(lNumbs)):
+                lNum = lNum - lNumbs[a] * pow(2, a)
+            else:
+                lNum = lNum + lNumbs[a] * pow(2, a)
+                
+        return lNum        
     else:
-
         #creates empty array and cast the base to an int
         lNumbs = []
         dBase = int(sBase)
@@ -88,8 +136,8 @@ def baseToDecimal(sNum, sBase):
         lNum = int(0)
         #every index of the list is multiplied by the base on the number (Index 0 is base^0 and so on)
         for a in range(0, len(lNumbs)):
-            lNum = lNum + lNumbs[a] * pow(dBase, a)
-
+                lNum = lNum + lNumbs[a] * pow(dBase, a)
+                
         return lNum
 
 
@@ -106,7 +154,7 @@ def inputs():
         while int(s1) > MAX_BASE or int(s1) <= MIN_BASE:
             s1 = input("Invalid base, try again: ")
     except ValueError:
-        while s1.upper() != "" and s1.upper() != "BCD":
+        while s1.upper() != "" and s1.upper() != "BCD" and s1.upper != "CP2":
             s1 = input("Invalid base, try again: ")
 
     s2 = input("Now type the base you wanna convert it to: ")
@@ -115,10 +163,11 @@ def inputs():
         while int(s2) > MAX_BASE or int(s2) <= MIN_BASE:
             s2 = input("Invalid base, try again: ")
     except ValueError:
-        while s2.upper() != "" and s2.upper() != "BCD":
+        while s2.upper() != "" and s2.upper() != "BCD" and s2.upper != "CP2":
             s2 = input("Invalid base, try again: ")
 
     print(s + "(" + s1 + ") = " + str(convertBase(s, s1, s2)) + "(" + s2 + ")")
 
-#print(decimalToBase("-10", "CP2"))
-inputs()
+#inputs()
+#print(decimalToBase("-2", "CP2"))
+print(baseToDecimal("110", "CP2"))
